6801a2ca6ed6a4ccabef768156cab35e2be3185f
Maksim <imyzhuk@gmail.com>
2024-10-06 13:56:22 +0300

    feat: add initial data to the database

---
+++ prisma/constants.ts
@@ -0,0 +1,223 @@
+import { Prisma } from '@prisma/client';
+
+export const locations = [
+  {
+    type: 'г',
+    name: 'Минск',
+    district: '',
+    region: '',
+    alias: 'minsk',
+  },
+  {
+    type: 'г',
+    name: 'Брест',
+    district: '',
+    region: 'Брестская',
+    alias: 'brest',
+  },
+  {
+    type: 'г',
+    name: 'Витебск',
+    district: '',
+    region: 'Витебская',
+    alias: 'vitebsk',
+  },
+  {
+    type: 'г',
+    name: 'Гомель',
+    district: '',
+    region: 'Гомельская',
+    alias: 'gomel',
+  },
+  {
+    type: 'г',
+    name: 'Гродно',
+    district: '',
+    region: 'Гродненская',
+    alias: 'grodno',
+  },
+  {
+    type: 'г',
+    name: 'Могилев',
+    district: '',
+    region: 'Могилевская',
+    alias: 'mogilev',
+  },
+  {
+    type: 'г',
+    name: 'Барановичи',
+    district: '',
+    region: 'Брестская',
+    alias: 'baranovichi',
+  },
+  {
+    type: 'г',
+    name: 'Бобруйск',
+    district: '',
+    region: 'Могилевская',
+    alias: 'bobrujsk',
+  },
+  {
+    type: 'г',
+    name: 'Борисов',
+    district: 'Борисовский',
+    region: 'Минская',
+    alias: 'borisov',
+  },
+  {
+    type: 'г',
+    name: 'Лида',
+    district: 'Лидский',
+    region: 'Гродненская',
+    alias: 'lida',
+  },
+  {
+    type: 'г',
+    name: 'Мозырь',
+    district: 'Мозырский',
+    region: 'Гомельская',
+    alias: 'mozyr',
+  },
+  {
+    type: 'г',
+    name: 'Молодечно',
+    district: 'Молодечненский',
+    region: 'Минская',
+    alias: 'molodechno',
+  },
+  {
+    type: 'г',
+    name: 'Пинск',
+    district: '',
+    region: 'Брестская',
+    alias: 'pinsk',
+  },
+  {
+    type: 'г',
+    name: 'Солигорск',
+    district: 'Солигорский',
+    region: 'Минская',
+    alias: 'soligorsk',
+  },
+  {
+    type: 'г',
+    name: 'Новополоцк',
+    district: '',
+    region: 'Витебская',
+    alias: 'novopolock',
+  },
+];
+
+export const additionalServicesCategoriesNames = [
+  'Перенавеска дверей',
+  'Надежная защита',
+  'Страхование',
+];
+
+export const initialReview = {
+  advantages: 'Цена , вместимость',
+  disadvantages: 'Шумность превышает 42дцб',
+  summary: 'Шумность превышает 42дцб , у меня 46 , а ему только 2 месяца',
+  rating: 0,
+  comment:
+    'Приносим извинения за доставленные неудобства. Благодаря Вашему отзыву, мы уточнили информацию о товаре у поставщика и исправили описание на сайте. Уровень шума 39 дБ. В результате вашего обращения, с Вами провели консультацию наши специалисты. Надеемся на понимание с Вашей стороны и на то, что у нас еще будет возможность произвести на Вас хорошее впечатление.',
+};
+
+export const refrigerator: Prisma.RefrigeratorCreateNestedOneWithoutProductInput =
+  {
+    create: {
+      type: 'холодильник с морозильником',
+      construction: 'двухдверный',
+      cameras: ['морозильная', 'холодильная'],
+      freezerLocation: 'снизу',
+      freezerSectionsCount: 2,
+      color: 'белый',
+      noFrostSystem: false,
+      energyClass: 'A',
+      compressorsCount: 1,
+      noiseLevel: 39,
+      retrodesign: false,
+      camerasCount: 2,
+      totalVolume: 173,
+      refrigeratorVolume: 131,
+      freezerVolume: 42,
+      multifunctionalArea: false,
+      dispenser: false,
+      minibar: false,
+      hingesLocation: 'перенавешиваемые',
+      lighting: true,
+      annualElectricityConsumption: 261,
+      inverterTechnology: false,
+      climateClass: 'N',
+      autonomousColdStorage: 14,
+      energySavingMode: false,
+      controlType: 'механическое',
+      controlUnitLocation: 'внутри',
+      independentTemperatureControl: false,
+      vacationMode: false,
+      childLock: false,
+      freshnessZone: false,
+      shelvesCount: 3,
+      shelfMaterial: 'стекло',
+      doorBalconiesCount: 3,
+      doorBalconiesMaterial: 'пластик',
+      superCooling: false,
+      forcedAirCirculation: false,
+      builtinOzonizer: false,
+      antibacterialProtection: false,
+      included: 'лоток для яиц',
+      freezingPower: 2,
+      height: 142.5,
+      width: 54.5,
+      depth: 60,
+      weight: 50,
+      originCountry: 'Беларусь',
+      producerInfo: 'ЗАО «АТЛАНТ», пр. Победителей, д. 61, г. Минск, РБ',
+      supplierInfo: 'ЗАО "АТЛАНТ", 220035, Минск, пр. Победителей 61',
+      description:
+        'ХМ 4208-000 — это узкий холодильник шириной 545 мм (вместо обычных 600 мм) и с классом энергоэффективности А из серии COMPACT. Создан на высоком дизайнерском и конструкторском уровне и соответствует современным тенденциям развития бытовой техники. С новым наружным и внутренним дизайном, позволяет по-современному решить проблему небольших кухонь.\nВ холодильной системе используется наиболее экономичный и экологически безопасный хладагент R600а (изобутан). Являясь природным веществом, он не оказывает негативного воздействия на окружающую среду.\nЭта модель из серии СOMPACT сохраняет свою работоспособность в диапазоне от 175 до 255 В, то есть устойчива к перепадам напряжения, что очень важно для многих потребителей в Беларуси, России, Украине.\nУправлять холодильник очень просто. Для включения достаточно вставить вилку шнура питания в розетку, подключив холодильник к электрической сети. При необходимости можно произвести регулировку температуры в холодильнике, используя ручку терморегулятора, которую устанавливают меткой на выбранное деление. Деление «1» соответствует наиболее высокой температуре (наименьшее охлаждение) в холодильнике, деление «4» — наиболее низкой (наибольшее охлаждение).',
+      additionalLinks: {
+        create: [
+          {
+            name: 'Руководство по эксплуатации',
+            link: 'https://cdn21vek.by/img/galleries/58/129/atlant_4208000_625e99c9c947e.pdf',
+            type: 'File',
+          },
+          {
+            name: 'Руководство по эксплуатации (особенности моделей)',
+            link: 'https://cdn21vek.by/img/galleries/58/129/atlant_4208000_625e99d8b1ff9.pdf',
+            type: 'File',
+          },
+          {
+            name: 'Сервисные центры ATLANT',
+            link: '/info/service_centres?producer=atlant',
+            type: 'Link',
+          },
+          {
+            name: 'Другая продукция бренда ATLANT',
+            link: 'https://www.21vek.by/info/brands/atlant',
+            type: 'Link',
+          },
+        ],
+      },
+    },
+  };
+
+export const refrigeratorImages: Prisma.ImageCreateManyProductInput[] = [
+  {
+    isMain: true,
+    url: 'https://cdn21vek.by/img/galleries/58/129/atlant_4208000_65bc97b9ca11d.jpeg',
+  },
+  {
+    isMain: false,
+    url: 'https://cdn21vek.by/img/galleries/58/129/atlant_4208000_56c302b5e778c.jpeg',
+  },
+  {
+    isMain: false,
+    url: 'https://cdn21vek.by/img/galleries/58/129/atlant_4208000_65bc97df5f0c3.jpeg',
+  },
+  {
+    isMain: false,
+    url: 'https://cdn21vek.by/img/galleries/58/129/atlant_4208000_65bc97c9b6e99.jpeg',
+  },
+];
---
+++ prisma/seed.ts
@@ -0,0 +1,186 @@
+import { PrismaClient } from '@prisma/client';
+import {
+  additionalServicesCategoriesNames,
+  locations,
+  initialReview,
+  refrigerator,
+  refrigeratorImages,
+} from './constants';
+import { prisma } from './prisma-client';
+
+async function up() {
+  await prisma.location.createMany({
+    data: locations.slice(1),
+  });
+
+  const location = await prisma.location.create({
+    data: locations[0],
+  });
+
+  const user = await prisma.user.create({
+    data: {
+      name: 'Test User',
+      email: 'user@example.com',
+      password: 'password',
+      phone: '+375291234567',
+      role: 'User',
+      locationId: location.id,
+    },
+  });
+
+  const category = await prisma.category.create({
+    data: {
+      name: 'Бытовая техника',
+      link: "/kitchen",
+    },
+  });
+
+  const subcategory = await prisma.category.create({
+    data: {
+      name: 'Крупная техника для кухни',
+      link: "/large_tech",
+      parentId: category.id,
+    },
+  });
+
+  const subSubcategory = await prisma.category.create({
+    data: {
+      name: 'Холодильники',
+      link: "/refrigerators",
+      parentId: subcategory.id,
+    },
+  });
+
+  const additionalServiceCategories = await Promise.all(
+    additionalServicesCategoriesNames.map((name) =>
+      prisma.additionalServiceCategory.create({
+        data: {
+          name,
+        },
+      }),
+    ),
+  );
+
+  const product = await prisma.product.create({
+    data: {
+      name: 'Холодильник с морозильником',
+      link: '/refrigerators/4208000_atlant',
+      preview:
+        'https://cdn21vek.by/img/galleries/58/129/preview/atlant_4208000_65bc97b9ca11d.jpeg',
+      discountTypes: ['Sale'],
+      isInStock: true,
+      model: 'ХМ 4208-000',
+      price: 794.7,
+      oldPrice: 883.0,
+      discount: 10,
+      rating: 4.6,
+      reviewsCount: 1,
+      categories: {
+        connect: [
+          { id: category.id },
+          { id: subcategory.id },
+          { id: subSubcategory.id },
+        ],
+      },
+      images: {
+        createMany: {
+          data: refrigeratorImages,
+        },
+      },
+      producer: {
+        create: {
+          name: 'Atlant',
+        },
+      },
+      additionalServices: {
+        create: [
+          {
+            name: 'Перенавеска 2 дверей холодильника без электронного управления на дверях',
+            description:
+              'При наличии электронных доводчиков стоимость перенавески дверей составит 45 руб., время работ – 1-1,5 часа. Производится в Барановичах, Бресте, Бобруйске, Борисове, Витебске, Гомеле, Гродно, Жлобине, Минске, Минском районе, Могилеве, Мозыре, Молодечно, Орше, Пинске, Полоцке.*Итоговая стоимость может измениться. Оплачивается только наличными по факту выполнения работ.',
+            price: 70.0,
+            priceCurrency: 'р',
+            category: {
+              connect: { id: additionalServiceCategories[0].id },
+            },
+          },
+          {
+            name: 'Надежная защита +1 год (500-999 руб.)',
+            description:
+              'Дополнительная поддержка вашей техники. В программу входит: увеличение гарантийного периода, бесплатный ремонт в негарантийных случаях, замена товара в случае отсутствия возможности ремонта, бесплатные консультации по вопросам эффективного использования товара, техническое обслуживание.',
+            link: '/info/extended_warranty',
+            price: 118.17,
+            priceCurrency: 'р',
+            category: {
+              connect: { id: additionalServiceCategories[1].id },
+            },
+          },
+          {
+            name: 'Надежная защита +2 года (500-999 руб.)',
+            description:
+              'Дополнительная поддержка вашей техники. В программу входит: увеличение гарантийного периода, бесплатный ремонт в негарантийных случаях, замена товара в случае отсутствия возможности ремонта, бесплатные консультации по вопросам эффективного использования товара, техническое обслуживание.',
+            link: '/info/extended_warranty',
+            price: 136.35,
+            priceCurrency: 'р',
+            category: {
+              connect: { id: additionalServiceCategories[1].id },
+            },
+          },
+          {
+            name: 'Страхование бытовой техники',
+            description:
+              'Комплексное страхование новой бытовой техники от хищения, гибели, а также от повреждения вследствие воздействия жидкостей, короткого замыкания, пожара, взрыва, удара молнии.',
+            link: '/services/insurance',
+            price: 45.45,
+            priceCurrency: 'р',
+            category: {
+              connect: { id: additionalServiceCategories[2].id },
+            },
+          },
+        ],
+      },
+      refrigerator: refrigerator,
+    },
+  });
+
+  const review = await prisma.review.create({
+    data: {
+      userId: user.id,
+      productId: product.id,
+      ...initialReview,
+    },
+  });
+}
+
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Location" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "AdditionalServiceCategory" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "AdditionalService" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Refrigerator" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "FavoriteItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ViewedItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "WaitedItem" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
